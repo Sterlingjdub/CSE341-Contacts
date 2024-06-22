d6c4ca3d7d950d69130606c63452767659ddc256
Sterling Welker <sterlingjdub@gmail.com>
2024-06-22 17:19:43 -0600

    Contacts files created

---
+++ routes/contacts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const contactsController = require('../controllers/contactsController');
+
+router.get('/', contactsController.getContacts);
+
+router.get('/:id', contactsController.getContact);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const mongodb = require('./data/database');
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+
+
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => (console.log(`Database is listening and running on port ${port}`)));
+    }
+});
+
